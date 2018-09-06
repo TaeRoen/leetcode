9c4535218c833a335d90a094aaa100b41d8b75c5
TaeRoen <TaeRoen@gmail.com>
2018-09-07 00:08:05 +0800

    add Hamming Distance

---
+++ 461.Hamming Distance/461.c
@@ -0,0 +1,11 @@
+int hammingDistance(int x, int y) {
+    int res = 0;
+    while (x != 0 || y != 0) {
+        if (x % 2 != y % 2) {
+            res++;
+        }
+        x /= 2;
+        y /= 2;
+    }
+    return res;
+}
