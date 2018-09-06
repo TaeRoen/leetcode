8e3841874eb35e0299f0d490dbff63a82aeebbc5
TaeRoen <TaeRoen@gmail.com>
2018-09-06 23:35:04 +0800

    add Maximum Subarray

--- 53.Maximum Subarray/53.c
+++ 53.Maximum Subarray/53.c
@@ -1,44 +1,15 @@
-int maxSubArray(int *nums, int numsSize)
-{
-    if (numsSize == 1)
-        return nums[0];
+int maxSubArray(int *nums, int numsSize) {
+    int max = nums[0], tmp_sum = nums[0];
 
-    int i = 0;
-    while (nums[i] == 0 && i < numsSize)
-    {
-        i++;
-    }
-    if (i == numsSize)
-        return 0;
-
-    int min_reverse = nums[i] < 0 ? nums[i] : 0;
-    int min = min;
-    int max = nums[i];
-    int max_num = nums[i];
-
-    for (; i < numsSize; i++)
-    {
-        if (max_num < nums[i])
-        {
-            max_num = nums[i];
+    for (int i = 1; i < numsSize; i++) {
+        if(tmp_sum > 0){
+            tmp_sum += nums[i];
+        }else{
+            tmp_sum = nums[i];
         }
-        nums[i] += nums[i - 1];
-        if (nums[i] < min_reverse)
-        {
-            min_reverse = nums[i];
-        }
-        if (nums[i] > max)
-        {
-            max = nums[i];
-            min = min_reverse;
+        if(max < tmp_sum){
+            max = tmp_sum;
         }
     }
-    if (max != nums[0])
-    {
-        return max - min;
-    }
-    else
-    {
-        return max_num;
-    }
+    return max;
 }
