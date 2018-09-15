d49a00e61f352c38bd43457a99bcba66be396d69
TaeRoen <TaeRoen@gmail.com>
2018-09-15 16:12:47 +0800

    add Insertion Sort List

---
+++ 147.Insertion Sort List/147.c
@@ -0,0 +1,38 @@
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *insertionSortList(struct ListNode *head) {
+    if(!head){
+        return 0;
+    }
+    struct ListNode *ret = head;
+    head = head->next;
+    ret->next = 0;
+
+    while (head) {
+        struct ListNode *current = head, *tmp = ret;
+        head = head->next;
+        if (ret->val > current->val) {
+            current->next = ret;
+            ret = current;
+            continue;
+        }
+
+        current->next = 0;
+        while (tmp->next) {
+            if (tmp->next->val > current->val) {
+                current->next = tmp->next;
+                tmp->next = current;
+                break;
+            }
+            tmp = tmp->next;
+        }
+        if (!current->next) {
+            tmp->next = current;
+        }
+        current->next = 0;
+    }
+    return ret;
+}
