c9a8870f720346443c6bb9c8230276005d12d04f
TaeRoen <TaeRoen@gmail.com>
2018-09-24 19:02:13 +0800

    add Rotate Image

---
+++ top-100-liked-questions-medium/48. Rotate Image/48.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    private void rotateSub(int[][] matrix, int sub) {
+        int start = sub;
+        int end = matrix.length - 1 - sub;
+        int tmp = matrix[start][start];
+        matrix[start][start] = matrix[end][start];
+        matrix[end][start] = matrix[end][end];
+        matrix[end][end] = matrix[start][end];
+        matrix[start][end] = tmp;
+        for (int i = 1; i < end - start; i++) {
+            matrix[i][i] = 0;
+            tmp = matrix[start][start + i];
+            matrix[start][start + i] = matrix[end - i][start];
+            matrix[end - i][start] = matrix[end][end - i];
+            matrix[end][end - i] = matrix[start + i][end];
+            matrix[start + i][end] = tmp;
+        }
+    }
+
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length / 2; i++) {
+            rotateSub(matrix, i);
+        }
+    }
+}
