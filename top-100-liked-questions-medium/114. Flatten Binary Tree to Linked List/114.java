aad10e192bd0ae510402fc034d299eefc84d136c
TaeRoen <TaeRoen@gmail.com>
2018-09-28 23:10:36 +0800

    add Flatten Binary Tree to Linked List

---
+++ top-100-liked-questions-medium/114. Flatten Binary Tree to Linked List/114.java
@@ -0,0 +1,39 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public class RightNode {
+        TreeNode head;
+        TreeNode tail;
+        RightNode(TreeNode node){
+            head = node;
+            tail = node;
+        }
+        void addRight(TreeNode node){
+            tail.left = null;
+            tail.right = node;
+            tail = node;
+        }
+    }
+    public void traverse(TreeNode root, RightNode node){
+        if(root){
+            node.addRight(root);
+            traverse(root.left, node);
+            traverse(root.riht, node);
+        }
+    }
+    public void flatten(TreeNode root) {
+        if(root){
+            RightNode node = new RightNode(root);
+            traverse(root.left, node);
+            traverse(root.right, node);
+        }
+    }
+}
