beb0d9b075dc4ea087904b5fd76130d8f2c77a50
TaeRoen <TaeRoen@gmail.com>
2018-10-03 23:04:05 +0800

    crlf to lf

--- top-100-liked-questions-easy/226. Invert Binary Tree/226.java
+++ top-100-liked-questions-easy/226. Invert Binary Tree/226.java
@@ -2,21 +2,24 @@ public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) { val = x; }
+
+    TreeNode(int x) {
+        val = x;
+    }
 }
 
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null){
+        if (root == null) {
             return root;
         }
         TreeNode tmp = root.left;
         root.left = root.right;
         root.right = tmp;
-        if(root.left != null)
-        invertTree(root.left);
-        if(root.right!= null)
-        invertTree(root.right);
+        if (root.left != null)
+            invertTree(root.left);
+        if (root.right != null)
+            invertTree(root.right);
         return root;
     }
 }
--- top-100-liked-questions-easy/448. Find All Numbers Disappeared in an Array/448.java
+++ top-100-liked-questions-easy/448. Find All Numbers Disappeared in an Array/448.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> res = new ArrayList<>();
-        if(nums.length == 0 || nums.length == 1){
+        if (nums.length == 0 || nums.length == 1) {
             return res;
         }
-        int n = nums.length+1;
+        int n = nums.length + 1;
         for (int i = 0; i < nums.length; i++) {
-            nums[nums[i]%n-1] += n;
+            nums[nums[i] % n - 1] += n;
         }
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i] < n ){
-                res.add(i+1);
+            if (nums[i] < n) {
+                res.add(i + 1);
             }
         }
         return res;
